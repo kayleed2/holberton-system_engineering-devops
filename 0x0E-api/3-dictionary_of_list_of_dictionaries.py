d0f1292d1c258a55686385ada53cf515374263db
Kaylee Dao <kayleed2@gmail.com>
2022-03-24 13:46:59 -0500

    Iterating through ids and through todos keys

---
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Uses a REST API to retrieve information"""
+import requests
+import json
+from os import sys
+
+
+user = requests.get("https://jsonplaceholder.typicode.com/users")
+todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+file = "todo_all_employees.json"
+
+with open(file, 'w') as f:
+    Dict = {}
+    for person in user.json():
+        id = person['id']
+        Dict[id] = []
+        for k in todos.json():
+            if k['userId'] == id:
+                dictionary = {"username": person['username'],
+                              "task": k['title'],
+                              "completed": k['completed']}
+                Dict[id].append(dictionary)
+    json.dump(Dict, f)
